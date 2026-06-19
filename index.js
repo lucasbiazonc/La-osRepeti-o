e285bfbc84fdbe583132e89650825bec65b8c2c9
lucasbiazonc <lucas.crosatti@escola.pr.gov.br>
2026-06-19 09:09:14 -0300

    calma

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-// run `node index.js` in the terminal
-
-console.log(`Hello Node.js v${process.versions.node}!`);
+let alunos = ['Artur','João', 'Jony', 'Kauan', 'Luisa', 'Matheus', 'Murilo', 'Nathan', 'Vini', 'Yuri'];
+let notas = [5, 6, 8, 4, 9, 3, 7, 10, 0, 6];
+let soma = notas.reduce((acumulador, valorAtual)=> acumulador + valorAtual,0)
+console.log(soma)
